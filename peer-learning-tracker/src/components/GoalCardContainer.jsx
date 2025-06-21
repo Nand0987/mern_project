6850ca4f861a85de5e95a8cca808f3d96a698b90
Nand0987 <103673462+Nand0987@users.noreply.github.com>
2025-06-21 18:44:02 +0530

    Update GoalCardContainer.jsx

--- peer-learning-tracker/src/components/GoalCardContainer.jsx
+++ peer-learning-tracker/src/components/GoalCardContainer.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { useLocation } from "react-router-dom";
 import GoalCard from "./GoalCard";
-import "../components/GoalCard.css";
+import "./goalcardstyle.css";
 import axios from "axios";
 
 export default function GoalCardContainer() {
